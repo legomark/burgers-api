849f1766be85313d369e666652858e39006da5bc
markligeti <ligetimark.e@gmail.com>
2021-11-20 22:39:59 +0100

    set up MongoDB

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require("config");
 const app = express();
 
 require("./startup/routes")(app);
+require("./startup/db")();
 
 const port = process.env.PORT || config.get("port");
 app.listen(port, () => {
