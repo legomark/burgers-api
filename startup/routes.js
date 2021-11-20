1605a0319a64347b9241b2975e502645cc23c714
markligeti <ligetimark.e@gmail.com>
2021-11-21 00:34:57 +0100

    implement user registration

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const places = require("../routes/places");
+const users = require("../routes/users");
 
 module.exports = function (app) {
     app.use(express.json());
     app.use("/api/places", places);
+    app.use("/api/users", users);
 };
