bb806f57377b16da06ab56ca3fb204bac4a293ce
markligeti <ligetimark.e@gmail.com>
2021-11-21 18:09:44 +0100

    set up helmet

--- startup/routes.js
+++ startup/routes.js
@@ -3,8 +3,10 @@ const places = require("../routes/places");
 const reviews = require("../routes/reviews");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
+const helmet = require("helmet");
 
 module.exports = function (app) {
+    app.use(helmet());
     app.use(express.json());
     app.use("/api/places", places);
     app.use("/api/reviews", reviews);
